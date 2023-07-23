200c472eefb07a086afe988857f5c93f16efe835
ledong18 <140312564+ledong18@users.noreply.github.com>
2023-07-23 19:21:54 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,1 +1,9 @@
+#!/usr/bin/env bash
+echo '初始变量'
+projectPath=/home/yunzhuangbei
+newTime=$(date +%Y%m%d%H%M%S)
+pass=$1 # 开发环境dev 测试环境test 生产环境prod
+echo '开始部署'$1
 
+cat code.* > code.zip
+unzip -P "${pass}" code.zip
